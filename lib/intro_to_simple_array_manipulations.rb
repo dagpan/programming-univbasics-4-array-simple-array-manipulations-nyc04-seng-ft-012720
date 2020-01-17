81f7986996cc8a624a4aa1ffbbbe7efbd74be60c
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-17 15:46:37 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,8 +15,13 @@
 #using_push
 
 def using_push(array1 = ["Spring", "Summer", "Fall"], season = "Winter")
-  array1. push(season)
+  array1.push(season)
 end
 
 
+#using_unshift
+
+def using_unshift(array1 = ["Spring", "Summer", "Fall"], season = "Winter")
+  array1.unshift(season)
+end
 

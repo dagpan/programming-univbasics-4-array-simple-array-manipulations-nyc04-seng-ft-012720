d13a042c717c89ce90030b9ac53170be06c204b1
Panagiotis Ntagkinis <panagiotisntagkinis@gmail.com>
2020-01-17 16:17:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -58,7 +58,7 @@ end
 
 #using_uniq
 
-def using_uniq(array1 = ["Spring", "Summer", "Fall"], element = "Winter")
+def using_uniq(array1 = ["Spring", "Summer", "Fall", "Winter"])
   array1.insert(3, element)
 end
 
